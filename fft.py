7b2c3b33089874be04b7c93e761707e700b6a625
andrewta999 <andrewta365@gmail.com>
2021-03-22 13:53:11 -0400

    finished mode 1

--- fft.py
+++ fft.py
@@ -1,10 +1,99 @@
+import argparse
+import math 
+import cv2 
 import numpy as np 
-import matplotlib as ml 
+import matplotlib.pyplot as plt 
+from matplotlib.colors import LogNorm 
+
+from ft_algorithm import FFT_2D, inverse_FFT_2D
+
+
+def get_new_size(s):
+    p = int(math.log(s, 2)) + 1
+    return int(pow(2, p))
+
+
+def resize_image(image):
+    # resize the image to power of 2
+    # Step 1: read the image
+    img = plt.imread(image).astype(float)
+    # Step 2: get image's sizes and new sizes
+    h, w = img.shape 
+    dim = (get_new_size(w), get_new_size(h))
+    # Step 3: resize the image
+    new_img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+    print("Original size: ", img.shape)
+    print("New size: ", new_img.shape)
+
+    # return resized image
+    return new_img 
+
+
+def mode1(image):
+    # Mode 1
+    # Step 1: resize image
+    img = resize_image(image)
+
+    # Step 2: perform 2D-FFT
+    new_img = FFT_2D(img)
+    # new_img1 = np.fft.fft2(img)
+
+    # Step 3: plot the results
+    fig, sub = plt.subplots(1, 2)
+    sub[0].imshow(img, plt.cm.gray)
+    sub[0].set_title('Original image')
+    # sub[0].imshow(np.abs(new_img1), norm=LogNorm())
+    # sub[0].set_title('FFT2')
+    sub[1].imshow(np.abs(new_img), norm=LogNorm())
+    sub[1].set_title('FFT on image')
+    fig.suptitle('Mode 1')
+    plt.show()
+
+def mode2(image):
+    pass 
+
+def mode3(image):
+    pass 
+
+def mode4(image):
+    pass 
+
+
+def parse_cli():
+    """
+    Parse a command 
+    Returns
+    -------
+    dict: 
+        A dictionary of parameters (mode, image)
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', action='store', dest='mode',
+                        help='Mode: [1] FFT, [2] Denoise, [3] Compress, [4] Plot', type=int, default=1)
+    parser.add_argument('-i', action='store', dest='image',
+                        help='Input image', type=str, default='moonlanding.png')
+    return parser.parse_args()
 
 
 def main():
-    pass 
-
+    # Parse commands from users
+    params = parse_cli()
+    # get mode and image
+    mode = params.mode
+    image = params.image  
+    # select mode
+    if mode == 1:
+        mode1(image)
+    elif mode == 2:
+        mode2(image)
+    elif mode == 3:
+        mode3(image)
+    elif mode == 4:
+        mode4(image)
+    else:
+        print("Mode must be between 1 and 4")
+    
 
 if __name__ == "__main__":
     main() 
---
+++ ft_algorithm.py
@@ -0,0 +1,118 @@
+import numpy as np 
+
+
+def FT_1D(img):
+    """
+    Naive 1D-DFT
+    Paramters
+    ---------
+    img: 1D-array
+    
+    Returns
+    -------
+    1D numpy array
+    """
+    # Step 1: get image size
+    N = img.shape[0]
+    result = np.zeros(N, dtype=complex)
+
+    # Step 2: perform DFT
+    for k in range(N):
+        temp = 0
+        for n in range(N):
+            temp += img[n] * np.exp(-2j * np.pi * k * n / N)
+        result[k] = temp
+    
+    return result 
+
+
+def inverse_FT_1D(img):
+    # Step 1: get image size
+    N = img.shape[0]
+    result = np.zeros(N, dtype=complex)
+
+    # Step 2: perform IDFT
+    for k in range(N):
+        temp = 0
+        for n in range(N):
+            temp += img[n] * np.exp(2j * np.pi * k * n / N)
+        result[k] = temp/N
+    
+    return result 
+
+
+def FFT_1D(img):
+    # Step 1: get image size
+    N = img.shape[0]
+    result = np.zeros(N, dtype=complex)
+
+    # Step 2: perform FFT
+    # threshold 
+    if N <= 16:
+        return FT_1D(img)
+    
+    # divide image into two equal parts
+    # and perform FFT on each part recursively
+    even = FFT_1D(img[::2])
+    odd = FFT_1D(img[1::2])
+
+    # merge two parts 
+    M = N//2
+    for k in range(N):
+        result[k] = even[k % M] + np.exp(-2j * np.pi * k / N) * odd[k % M]
+    return result
+
+
+def inverse_FFT_1D(img):
+    # Step 1: get image size
+    N = img.shape[0]
+    result = np.zeros(N, dtype=complex)
+
+    # Step 2: perform FFT
+    # threshold 
+    if N <= 32:
+        return inverse_FT_1D(img)
+    
+    # divide image into two equal parts
+    # and perform FFT on each part recursively
+    even = inverse_FFT_1D(img[::2])
+    odd = inverse_FFT_1D(img[1::2])
+
+    # merge two parts 
+    M = N//2
+    for k in range(N):
+        result[k] = even[k % M] + np.exp(2j * np.pi * k / N) * odd[k % M]
+        result[k] /= N
+    return result
+
+
+def FFT_2D(img):
+    # Step 1: get row and column
+    R, C = img.shape 
+    result = np.zeros((R, C), dtype=complex)
+
+    # Step 2: perform Fourier transform on row
+    for r in range(R):
+        result[r, :] = FFT_1D(img[r, :])
+
+    # Step 3: perform Fourier transform on column
+    for c in range(C):
+        result[:, c] = FFT_1D(result[:, c])
+
+    return result 
+
+
+def inverse_FFT_2D(img):
+    # Step 1: convert to 2D np-array
+    R, C = img.shape
+    result = np.zeros((R, C), dtype=complex)
+
+    # Step 2: perform Fourier transform on row
+    for r in range(R):
+        result[r, :] = inverse_FFT_1D(img[r, :])
+
+    # Step 3: perform Fourier transform on column
+    for c in range(C):
+        result[:, c] = inverse_FFT_1D(result[:, c])
+
+    return result 
